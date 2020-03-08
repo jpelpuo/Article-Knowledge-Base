a8a5ad4ebe0547f72d88fad1fcd12da26f28be59
Jamal Pelpuo <jpelpuo@ymail.com>
2020-03-08 00:16:52 +0000

    Adds more code and styles

--- app.js
+++ app.js
@@ -3,30 +3,26 @@ const path = require('path');
 const chalk = require('chalk');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { addArticle } = require('./services/article');
+const { addArticle, getArticle, editArticle, getEveryArticle, deleteArticle } = require('./services/article');
 
 
-    //Connect to MongoDB
-    (async () => {
-        try {
-            await mongoose.connect('mongodb://localhost:27017/nodekb',
-                {
-                    useNewUrlParser: true,
-                    useUnifiedTopology: true
-                });
-            // console.log(chalk.greenBright("Connected to database"));
-        } catch (e) {
-            throw new Error("Error connecting to db")
-        }
-    })().then(() => {
-        console.log(chalk.greenBright("Connected to database"))
-    }).catch(error => {
-        console.log(error)
-    });
-
-let db = mongoose.connection;
-
-const Article = require('./models/article');
+//Connect to MongoDB
+(async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/nodekb',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
+        // console.log(chalk.greenBright("Connected to database"));
+    } catch (e) {
+        throw new Error(chalk.redBright("Error connecting to db"))
+    }
+})().then(() => {
+    console.log(chalk.greenBright("Connected to database"))
+}).catch(error => {
+    console.log(error)
+});
 
 //Initialise app
 const app = express();
@@ -42,18 +38,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+//Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Home route
 app.get('/', (request, response) => {
-    Article.find({}, (error, articles) => {
-        if (error) {
-            console.log(error);
-        } else {
+    getEveryArticle()
+        .then(articles => {
             response.render('index', {
                 title: "Articles",
                 articles
             });
-        }
-    })
+        }).catch(error => {
+            console.log(chalk.redBright(error));
+        })
     console.log(request);
 })
 
@@ -81,6 +79,67 @@ app.post('/articles/add', (request, response) => {
             return;
         })
 })
+
+// Get single article
+app.get('/article/:id', (request, response) => {
+    let id = request.params.id;
+    getArticle(id)
+        .then(article => {
+            response.render('article', {
+                article: article
+            })
+        })
+        .catch(error => {
+            console.log(error)
+        })
+})
+
+// Load edit form
+app.get('/article/edit/:id', (request, response) => {
+    let id = request.params.id;
+    getArticle(id)
+        .then(article => {
+            response.render('edit_article', {
+                title: "Edit Article",
+                article: article
+            })
+        })
+        .catch(error => {
+            console.log(error)
+        })
+})
+
+
+// Update Submit POST article
+app.post('/articles/edit/:id', (request, response) => {
+    let article = {};
+    article.title = request.body.title;
+    article.author = request.body.author;
+    article.body = request.body.body;
+
+    editArticle(request.params.id, article)
+        .then(article => {
+            response.redirect('/');
+            console.log(article);
+        })
+        .catch(error => {
+            console.log(error)
+            return;
+        })
+})
+
+//Delete post
+app.delete('/article/:id', (request, response) => {
+    let id = request.params.id;
+    deleteArticle(id)
+    .then(article => {
+        response.send('Success');
+    })
+    .catch(error => {
+        console.log(error)
+    })
+})
+
 
 //Create server
 app.listen('3000', () => {
--- services/article.js
+++ services/article.js
@@ -1,17 +1,50 @@
 const Article = require('../models/article');
 
-const addArticle = async ({title, author, body}) => {
+const getEveryArticle = async () => {
     try{
-        return await new Article({title, author, body}).save((error) => {
-            if(error){
-                throw new Error("Document not saved");
-            }else{
-                console.log("Saved");
-            } 
-        })
+        return await Article.find({})
     }catch(e){
-        throw new Error("Error")
+        throw new Error(e);
     }
 }
 
-module.exports.addArticle = addArticle;
+const addArticle = async ({title, author, body}) => {
+    try{
+        return await new Article({title, author, body}).save();
+    }catch(e){
+        throw new Error(e)
+    }
+}
+
+const getArticle = async (id) => {
+    try{
+        return await Article.findById(id);
+    }catch(e){
+        throw new Error(e)
+    }
+}
+
+const editArticle = async (id, data) => {
+    let query = {_id: id}
+    try{
+        return await Article.update(query, data);
+    }catch(e){
+        throw new Error(e)
+    }
+}
+
+const deleteArticle = async id => {
+    let query = {_id: id};
+    try{
+        return await Article.remove(query);
+    }catch(e){
+        throw new Error(e);
+    }
+}
+module.exports = {
+    addArticle,
+    getArticle,
+    editArticle,
+    getEveryArticle,
+    deleteArticle
+}

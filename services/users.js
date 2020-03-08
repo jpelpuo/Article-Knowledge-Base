f74c1b80f8539bed94feb1117563af4acb75d3ef
Jamal Pelpuo <jpelpuo@ymail.com>
2020-03-08 21:51:35 +0000

    Fixes user registration bug

--- services/users.js
+++ services/users.js
@@ -2,27 +2,24 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 
 
-const registerUser = async ({ name, email, username, password }) => {
-    try {
-        return bcrypt.genSalt(10, (error, salt) => {
-            if (error) {
+const registerUser = async ({name, email, username, password}) => {    
+    bcrypt.genSalt(10, (error, salt) => {
+        if(error){
+            console.log(error);
+        }
+        bcrypt.hash(password, salt, async (error, hash) => {
+            if(error){
                 console.log(error);
-            }
-            bcrypt.hash(password, salt, async (error, hash) => {
-                if (error) {
-                    console.log(error);
-                } else {
-                    try {
-                        return await new User({ name, email, username, hash }).save();
-                    } catch (e) {
-                        throw new Error(e);
-                    }
+            }else{
+                try{
+                    let password = hash;
+                    return await new User({name, email, username, password}).save();
+                }catch(e){
+                    throw new Error(e);
                 }
-            })
+            }
         })
-    } catch (e) {
-
-    }
+    })
 }
 
 module.exports = {
